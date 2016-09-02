e0ed78063201edc85795c5d8a5475449f5d1ae37
thamires_garcia <nico1805>
2016-09-02 16:35:53 -0300

    Colisao boss

--- Source/Atividade/Boss.cpp
+++ Source/Atividade/Boss.cpp
@@ -2,6 +2,8 @@
 
 #include "Atividade.h"
 #include "Boss.h"
+#include "MyCharacter.h"
+#include "ProjectileActor.h"
 
 
 // Sets default values
@@ -10,14 +12,15 @@ ABoss::ABoss()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Root = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
-
+	Root = CreateDefaultSubobject<USphereComponent>(TEXT("Root"));
+	Root->OnComponentBeginOverlap.AddDynamic(this, &ABoss::OnOverlapBegin);
 	RootComponent = Root;
-
-
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->AttachTo(RootComponent);
 
+	FVector Inicio = GetActorLocation();
+
+	BossLife = 10;
 	Count = 0;
 }
 
@@ -32,8 +35,9 @@ void ABoss::BeginPlay()
 void ABoss::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-	FVector Inicio = GetActorLocation();
 	FVector LocalizacaoAtual = GetActorLocation();
+	FString HP= FString::Printf(TEXT("Life: %d"),GetBossLife());
+	
 
 	if (Count >= 0 && Count<200) {
 		LocalizacaoAtual.Y++;
@@ -44,13 +48,13 @@ void ABoss::Tick( float DeltaTime )
 	else if (Count>=400 && Count <900) {
 		LocalizacaoAtual.Y++;
 	}
-	else if (Count >= 900 && Count <1100) {
+	else if (Count >= 900 && Count <1200) {
 		LocalizacaoAtual.X++;
 	}
-	else if (Count >= 1100 && Count <1800) {
+	else if (Count >= 1200 && Count <1900) {
 		LocalizacaoAtual.Y--;
 	}
-	else if (Count >= 1800 && Count <2100) {
+	else if (Count >= 1900 && Count <2400) {
 		LocalizacaoAtual.X--;
 	}else{
 		Count = 0;
@@ -61,6 +65,38 @@ void ABoss::Tick( float DeltaTime )
 	Count++;
 	SetActorLocation(LocalizacaoAtual);
 
+}
+
+
+void ABoss::SetBossLife(int NewLife) {
+	BossLife = NewLife;
+}
+
+int ABoss::GetBossLife() {
+	if (BossLife > 10) {
+		Destroy();
+	}
+	return BossLife;
+}
+
+void ABoss::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	if ((OtherActor != nullptr) && (OtherActor != this) &&
+		(OtherComp != nullptr) && (OtherActor->IsA(AMyCharacter::StaticClass()))) {
+
+		AMyCharacter* MyCharacter = Cast<AMyCharacter>(OtherActor);
+		MyCharacter->SetLife(0);
+	}
+
+	if ((OtherActor != nullptr) && (OtherActor != this) &&
+		(OtherComp != nullptr) && (OtherActor->IsA(AProjectileActor::StaticClass()))) {
+		
+		AProjectileActor* Projectile = Cast<AProjectileActor>(OtherActor);
+		
+		SetBossLife(GetBossLife()-1);
+
+		
+		
+	}
 
 }
 
